7aa3e5117373dfdec7ebce5c739578b08fdf715f
ekdms6 <uncas97@naver.com>
2024-11-26 18:12:11 +0900

    Feat: mapper 작성
    BoardMapper.xml, BoardMapper.java, BoardMapperTests.java 작성

---
+++ src/test/java/net/fiveleaves/mapper/BoardMapperTests.java
@@ -0,0 +1,82 @@
+package net.fiveleaves.mapper;
+
+import org.apache.ibatis.annotations.Mapper;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+
+import lombok.extern.log4j.Log4j;
+import net.fiveleaves.domain.BoardDTO;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations="file:**/*-context.xml")
+@WebAppConfiguration
+@Log4j
+public class BoardMapperTests {
+	
+	@Autowired
+	private BoardMapper boardMapper;
+	
+	@Test
+	public void testGetList() {
+		boardMapper.getList().forEach(boardDto -> log.info(boardDto));
+	}
+	
+	@Test
+	public void testInsert() {
+		BoardDTO boardDto = new BoardDTO();
+		boardDto.setBoardTitle("삽입테스트제목");
+		boardDto.setBoardContent("삽입테스트내용");
+		boardDto.setClubNo(1L);
+		boardDto.setUserNo(1L);
+		
+		boardMapper.insert(boardDto);
+		
+		log.info(boardDto);
+		
+	}
+	
+	@Test
+	public void testInsertSelectKey() {
+		BoardDTO boardDto=new BoardDTO();
+		boardDto.setBoardTitle("번호지정삽입테스트제목");
+		boardDto.setBoardContent("번호지정삽입테스트내용");
+		boardDto.setClubNo(1L);
+		boardDto.setUserNo(1L);
+		
+		boardMapper.insertSelectKey(boardDto);
+		
+		log.info(boardDto);
+	}
+	
+	@Test
+	public void testRead() {
+		BoardDTO board = boardMapper.read(3L);
+		log.info(board);
+		
+	}
+	
+	@Test
+	public void testDelete() {
+		log.info(boardMapper.delete(8L));
+				
+	}
+	
+	@Test
+	public void testUpdate() {
+		BoardDTO boardDto= new BoardDTO();
+		boardDto.setBoardNo(7L);
+		boardDto.setBoardTitle("게시물 제목");
+		boardDto.setBoardContent("수정된 게시물 내용");
+		boardDto.setUserNo(1L);
+		boardDto.setClubNo(1L);
+		
+		int count=boardMapper.update(boardDto);
+		log.info("UPDATE COUNT: " + count);
+	}
+	
+
+}
