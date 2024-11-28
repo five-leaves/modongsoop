6201f5ae6bae78bb5c3acd7fc98034a19488f3c4
hwakyung <hwakyung5531@gmail.com>
2024-11-28 09:13:08 +0900

    Feat: 동호회 목록 페이징 추가중

--- src/main/java/net/fiveleaves/mapper/ClubMapper.java
+++ src/main/java/net/fiveleaves/mapper/ClubMapper.java
@@ -7,6 +7,7 @@ import net.fiveleaves.domain.ClubLogDTO;
 
 public interface ClubMapper {
 	public List<ClubDTO> getAllClubList();				// 동호회 전체 목록
+	public List<ClubDTO> getAllClubListWithPaging();	// 동호회 전체 목록 페이징	
 	public List<ClubDTO> getMyClubList(Long userNo);	// 가입한 동호회 목록
 	public ClubDTO readClub(Long clubNo);				// 동호회 정보
 	public Long countMember(Long clubNo);				// 동호회 회원수
@@ -14,6 +15,6 @@ public interface ClubMapper {
 	public int joinClub(ClubLogDTO clubLog);			// 동호회 가입
 	public int updateClub(ClubDTO club);				// 동호회 정보 수정
 	public int deleteClub(Long clubNo);					// 동호회 삭제
-	public int deleteMember(ClubLogDTO clubLog);	// 동호회 회원 삭제
+	public int deleteMember(ClubLogDTO clubLog);		// 동호회 회원 삭제
 	public int deleteAllMember(Long clubNo);			// 동호회 전체 회원 삭제
 }
